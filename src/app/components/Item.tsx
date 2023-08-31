a46976f7d3cb1511ecbfa814bd1c6ab5a32e986a
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-08-31 11:12:42 -0300

    feat: fix opacity in done items

--- src/app/components/Item.tsx
+++ src/app/components/Item.tsx
@@ -23,9 +23,9 @@ export default function Item(props: Item) {
   }
 
   return (
-    <div  className={`bg-gray-400 p-4 border-[1px] border-gray-300 rounded-lg flex items-center justify-between flex-1 ${props.done && 'opacity-50'} `}>
+    <div  className={`bg-gray-400 p-4 border-[1px] border-gray-300 rounded-lg flex items-center justify-between flex-1 ${props.done && 'bg-gray-500 border-gray-400'} `}>
       <div
-        className='border-purple-light bg-gray-400 flex items-center gap-4'
+        className={`border-purple-light bg-gray-400 flex items-center gap-4 ${props.done && 'bg-gray-500'}`}
       >
         <input 
           type="checkbox"
@@ -33,11 +33,11 @@ export default function Item(props: Item) {
           name={props.item}
           id={props.id.toString()}
           checked={props.done}
-          className='appearance-none cursor-pointer w-6 h-6 border-2 rounded-md border-purple-light hover:bg-purple-dark checked:bg-success checked:border-success checked:hover:bg-success-light checked:hover:border-success-light transition-all duration-200 checked:after:content-["✓"] items-center justify-center flex text-white'
+          className={`appearance-none cursor-pointer w-6 h-6 border-2 rounded-md border-purple-light hover:bg-purple-dark checked:bg-success checked:border-success checked:hover:bg-success-light checked:hover:border-success-light transition-all duration-200 checked:after:content-["✓"] items-center justify-center flex text-white ${props.done && 'opacity-50'}`}
         />
         <div>
-          <p className={`text-gray-100 text-sm font-bold capitalize ${props.done && 'line-through'}`}>{props.item}</p>
-          <p className='text-gray-200 text-xs'>
+          <p className={`text-gray-100 text-sm font-bold capitalize ${props.done && 'line-through text-gray-200'}`}>{props.item}</p>
+          <p className={`text-gray-200 text-xs ${props.done && 'opacity-50'}`}>
             {props.quantity == 1 && props.measure === 'unit' && `${props.quantity} unidade`}
             {props.quantity != 1 && props.measure === 'unit' && `${props.quantity} unidades`}
             {props.quantity == 1 && props.measure === 'liter' && `${props.quantity} litro`}
@@ -48,38 +48,38 @@ export default function Item(props: Item) {
         </div>
       </div>
       
-      <div className='flex items-center gap-3'>
+      <div className={`flex items-center gap-3`}>
         {
           props.category === 'fruit' &&
-          <div className='bg-orange-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2'>
+          <div className={`bg-orange-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2 ${props.done && 'opacity-50'}`}>
             <Apple className='text-orange h-4 w-4'/>
             <span className='text-orange text-xs font-semibold hidden sm:block'>fruta</span>
           </div>
         }
         {
           props.category === 'bakery' &&
-          <div className='bg-yellow-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2'>
+          <div className={`bg-yellow-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2 ${props.done && 'opacity-50'}`}>
             <Sandwich className='text-yellow h-4 w-4'/>
             <span className='text-yellow text-xs font-semibold hidden sm:block'>padaria</span>
           </div>
         }
         {
           props.category === 'vegetable' &&
-          <div className='bg-green-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2'>
+          <div className={`bg-green-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2 ${props.done && 'opacity-50'}`}>
             <Carrot className='text-green h-4 w-4'/>
             <span className='text-green text-xs font-semibold hidden sm:block'>legume</span>
           </div>
         }
         {
           props.category === 'drink' &&
-          <div className='bg-blue-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2'>
+          <div className={`bg-blue-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2 ${props.done && 'opacity-50'}`}>
             <Milk className='text-blue h-4 w-4'/>
             <span className='text-blue text-xs font-semibold hidden sm:block'>bebida</span>
           </div>
         }
         {
           props.category === 'meat' &&
-          <div className='bg-pink-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2'>
+          <div className={`bg-pink-dark p-2 sm:px-4 sm:py-2 rounded-full flex items-center gap-1 sm:gap-2 ${props.done && 'opacity-50'}`}>
             <Beef className='text-pink h-4 w-4'/>
             <span className='text-pink text-xs font-semibold hidden sm:block'>carne</span>
           </div>
@@ -89,7 +89,7 @@ export default function Item(props: Item) {
             <MoreVertical className={`text-purple-light hover:text-purple w-5 h-5 transition-all duration-200 ${showOptions && 'text-purple rotate-90'}`} />
           </button>
           {showOptions && (
-            <div className='absolute top-0 right-5'>
+            <div className='absolute top-0 right-6 brightness-125'>
               <button 
                 onClick={props.handleDeleteItem} 
                 className='bg-gray-400 hover:bg-gray-300 p-3 text-gray-200 hover:text-gray-100 text-sm border-[1px] border-gray-300 rounded-t-md transition-all duration-200 w-32 '
